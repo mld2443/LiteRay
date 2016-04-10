683369b365bd317bd2de5e76ee6440b95643c5b6
Matthew Dillard <7395051@gmail.com>
2016-04-10 13:41:17 -0500

    Initial Commit

---
+++ LiteRay/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LiteRay
+//
+//  Created by Matthew Dillard on 4/10/16.
+//  Copyright © 2016 Matthew Dillard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
